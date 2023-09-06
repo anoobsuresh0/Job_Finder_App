4af14f16d3e013c0069080ce1d42ebcaae5a4cea
Anoob Suresh <anoob.suresh@perleybrook.com>
2023-09-06 22:31:23 +0530

    Finished Job Search Website

--- src/Components/ValueDiv/Value.jsx
+++ src/Components/ValueDiv/Value.jsx
@@ -6,7 +6,7 @@ const Value = () => {
       <h1 className='text-textColor text-[25px] py-[2rem] pb-[3rem] font-bold w-[400px] block ' >The value that hold us true and to account</h1>
       <div className="grid gap-[10rem] grid-cols-3 items-center ">
 
-        <div className="singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]  ">
+        <div className="singleGrid rounded-[10px] hover:bg-[#a8faa0] p-[1.5rem]  ">
           <div className="flex items-center gap-3 ">
             <div className="imgDiv p-[4px] rounded-[.8rem] bg-[#dedef8] h-[40px] w-[40px] flex items-center justify-center  ">
               <img src="https://www.freeiconspng.com/thumbs/xbox-icon/xbox-logo-icon-5.png" alt="" className='w-[70%]' />
